d75d0d67e32ecc55a148b66001ac7153652d6505
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-07-31 20:12:58 -0300

    feat: path + fs

---
+++ index.js
@@ -0,0 +1,6 @@
+const { Person } = require('./person.js')
+
+// require('./modules/path.js')
+require('./modules/fs')
+
+const person = new Person('Marcos')
---
+++ modules/fs.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+
+// Criar uma pasta
+/* fs.mkdir(path.join(__dirname, '/test'), (error) => {
+  if (error) {
+    return  console.error('Erro: ', error)
+  }
+
+  console.log('Pasta criada com sucesso')
+}) */
+
+// Criar um arquivo
+fs.writeFile(
+  path.join(__dirname, '/test', 'test.html'),
+  'hello node!',
+  (error) => {
+    if (error) {
+      return console.error("Erro: ", error)
+    }
+
+    console.log("Arquivo criado com sucesso!")
+    
+    // Adicionar à um arquivo
+    fs.appendFile(path.join(__dirname, '/test', 'test.html'), 'hello world!', (error) => {
+      if (error) {
+        return console.error("Erro: ", error)
+      }
+    
+      console.log("Arquivo modificado com sucesso!")
+    })
+
+    // Ler arquivo
+    fs.readFile(path.join(__dirname, '/test', 'test.html'), 'utf-8', (error, data) => {
+      if (error) {
+        return console.error("Erro: ", error)
+      }
+
+      console.log(data)
+    })
+  }
+)
