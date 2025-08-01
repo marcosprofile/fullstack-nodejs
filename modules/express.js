77273b998a4b9a62b764139634fc117914d41111
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-07-31 23:29:05 -0300

    feat: middleware + return users in html with ejs + components with ejs

--- modules/express.js
+++ modules/express.js
@@ -5,9 +5,21 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/home', (req, res) => {
-  res.contentType('application/html')
-  res.status(200).send('<h1>Hello World!</h1>')
+app.set('view engine', 'ejs')
+app.set('views', 'src/views/')
+
+app.use((req, res, next) => {
+  console.log(`Request Type: ${req.method}`)
+  console.log(`Content Type: ${req.headers["content-type"]}`)
+  console.log(`Date: ${new Date()}`)
+
+  next()
+})
+
+app.get('/views/users', async (req, res) => {
+  const users = await UserModel.find({})
+
+  res.render('index', { users })
 })
 
 app.get('/users', async (req, res) => {
